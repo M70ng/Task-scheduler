027d0e930e5725aa9afab66f1c952372772c3af0
M70ng <mion23129@gmail.com>
2025-10-05 15:35:49 -0700

    add iterator, stack

--- src/Stack.h
+++ src/Stack.h
@@ -10,43 +10,55 @@
 #include <stdexcept>
 
 template <typename T>
-class Stack {
+class Stack
+{
 private:
-    Node<T>* top;
+    Node<T> *top;
     int size;
 
 public:
-    Stack() {
+    Stack()
+    {
         top = nullptr;
         size = 0;
     }
 
-    ~Stack() {
-        while (!isEmpty()) {
-            pop();
-        }
+    ~Stack()
+    {
+        clear();
     }
 
-    bool isEmpty() const {
+    bool isEmpty() const
+    {
         return size == 0;
     }
 
-    int getSize() const {
+    int getSize() const
+    {
         return size;
     }
 
-    void push(const T& val) {
-        Node<T>* newNode = new Node<T>(val);
+    void clear()
+    {
+        while (!isEmpty())
+            pop();
+    }
+
+    void push(const T &val)
+    {
+        Node<T> *newNode = new Node<T>(val);
         newNode->next = top;
         top = newNode;
         size++;
     }
 
-    T pop() {
-        if (isEmpty()) {
+    T pop()
+    {
+        if (isEmpty())
+        {
             throw std::runtime_error("Stack underflow");
         }
-        Node<T>* temp = top;
+        Node<T> *temp = top;
         T value = temp->data;
         top = top->next;
         delete temp;
@@ -54,8 +66,10 @@ public:
         return value;
     }
 
-    T peek() const {
-        if (isEmpty()) {
+    T peek() const
+    {
+        if (isEmpty())
+        {
             throw std::runtime_error("Stack is empty");
         }
         return top->data;
--- src/TaskIterator.h
+++ src/TaskIterator.h
@@ -1,12 +1,10 @@
-//  Node* current
-
-// Node* head
-
 #ifndef TASKITERATOR_H
 #define TASKITERATOR_H
 
+#include <iostream>
 #include "Node.h"
 #include "Task.h"
+using namespace std;
 
 class TaskIterator {
 private:
@@ -16,8 +14,33 @@ private:
 public:
     TaskIterator(Node<Task>* head);
     bool hasNext() const;
-    Task* next();
+    Task next();
     void reset();
+    bool isEnd() const; 
 };
+
+TaskIterator::TaskIterator(Node<Task>* head) : current(head), start(head) {}
+
+bool TaskIterator::hasNext() const {
+    return current != nullptr;
+}
+
+Task TaskIterator::next() {
+    if (!current) {
+        cerr << "Iterator error: no more elements.\n";
+        return Task();
+    }
+    Task data = current->data;
+    current = current->next;
+    return data;
+}
+
+void TaskIterator::reset() {
+    current = start;
+}
+
+bool TaskIterator::isEnd() const {
+    return current == nullptr;
+}
 
 #endif // TASKITERATOR_H
